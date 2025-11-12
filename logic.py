a0b22f33efba974045e645a606d864630d07b1b0
TEA0504 <turkayerenaltunsoy0504@gmail.com>
2025-11-12 21:01:06 +0300

    first commite

---
+++ logic.py
@@ -0,0 +1,24 @@
+import aiohttp
+import random
+
+class Pokemon:
+    pokemons = {}
+
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.img = None
+        self.name = None
+        self.attackpts = None
+        self.defense = None
+        self.hp = None
+
+    async def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data['forms'][0]['name']
+                else:
+                    return "Pikachu"
---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override = True)
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intent.all()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents = intents)
+
+@bot.event
+
+async def on_read():
+    print(f"{bot.user.name} is ready")
+
+bot.run(TOKEN)
