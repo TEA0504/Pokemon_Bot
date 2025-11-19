10565a3fbe8ae15600a76446d9d952683756576e
TEA0504 <turkayerenaltunsoy0504@gmail.com>
2025-11-19 20:18:24 +0300

    Intents

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 import discord
 from discord.ext import commands
 
-intents = discord.Intent.all()
+intents = discord.Intents.all()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents = intents)
@@ -26,5 +26,17 @@ async def go(ctx):
     if author not in Pokemon.pokemons.keys():
         pokemon = Pokemon(author)
         Pokemon.pokemons[author] = pokemon
+        await pokemon.fetch_stats()
+        await pokemon.set_stats()
+        await ctx.send(await pokemon.info())
+        image_url = await pokemon.show_img()
+        if image_url:
+            embed = discord.Embed()
+            embed.set_image(url=image_url)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Pokémonun görüntüsü yüklenemedi!")
+    else:
+        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")
 
 bot.run(TOKEN)
